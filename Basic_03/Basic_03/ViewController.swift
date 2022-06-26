db018f4293c51dc30d970b923be2cf3421d6aee9
k906506 <k906506@naver.com>
2022-06-26 15:53:19 +0900

    [feat] 숫자 클릭시 뷰 변경되도록 구현

--- Basic_03/Basic_03/ViewController.swift
+++ Basic_03/Basic_03/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation {
+    case Add
+    case Subtract
+    case Multiply
+    case Divide
+    case Unknown
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOp = ""
+    var secondOp = ""
+    var currentOp : Operation = .Unknown
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return }
+        if self.displayNumber.count < 9 {
+            if numberValue == "0", self.displayNumber.isEmpty {
+                return
+            }
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOp = ""
+        self.secondOp = ""
+        self.result = ""
+        self.currentOp = .Unknown
+        self.numberOutputLabel.text = "0"
+    }
+    
+    
+    @IBAction func tapDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+        }
+        self.numberOutputLabel.text = self.displayNumber
+    }
 }
 
