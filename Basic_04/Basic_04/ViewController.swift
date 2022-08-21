8b6e932c815f58260c6417dada5a08ae7c46692f
k906506 <k906506@naver.com>
2022-08-21 14:58:02 +0900

    [feat] dequeueReusable로 재사용가능한 TableView 구현

--- Basic_04/Basic_04/ViewController.swift
+++ Basic_04/Basic_04/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -21,13 +22,39 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tabAddButton(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title : "할 일 등록", message:"할 일을 입력해주세요.", preferredStyle: .alert)
-        let registerButton = UIAlertAction(title:"등록", style: .default, handler: { _ in })
-        
+        let alert = UIAlertController(title : "할 일 등록", message:nil, preferredStyle: .alert)
+        let registerButton = UIAlertAction(title:"등록", style: .default, handler: {[weak self] _ in guard let title = alert.textFields?[0].text
+                                                                                                            
+            else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            self?.tableView.reloadData()
+        })
         let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler : nil)
+        
         alert.addAction(cancelButton)
         alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: {
+            textField in textField.placeholder = "할 일을 입력해주세요"
+        })
+        
         self.present(alert, animated: true, completion: nil)
     }
 }
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView : UITableView, numberOfRowsInSection section : Int) -> Int {
+        return 0    }
+    
+    func tableView(_ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for : indexPath)
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+    }
+}
+
+
+
+
 
