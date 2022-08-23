a259e9b2bd0293e97a66c00f6ad0126b340b688a
k906506 <k906506@naver.com>
2022-08-23 17:33:24 +0900

    [feat] 할 일 클릭시 우측에 체크마크가 보여지는 기능 추가

--- Basic_04/Basic_04/ViewController.swift
+++ Basic_04/Basic_04/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var tasks = [Task]()
+    var tasks = [Task]() {
+        didSet {
+            self.saveTasks()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.loadDatas()
     }
     
     @IBOutlet weak var tableView: UITableView!
@@ -23,9 +28,8 @@ class ViewController: UIViewController {
     
     @IBAction func tabAddButton(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title : "할 일 등록", message:nil, preferredStyle: .alert)
-        let registerButton = UIAlertAction(title:"등록", style: .default, handler: {[weak self] _ in guard let title = alert.textFields?[0].text
-                                                                                                            
-            else { return }
+        let registerButton = UIAlertAction(title:"등록", style: .default, handler: {[weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
             let task = Task(title: title, done: false)
             self?.tasks.append(task)
             self?.tableView.reloadData()
@@ -40,21 +44,51 @@ class ViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
+    
+    func saveTasks() {
+        let data = self.tasks.map {
+            [
+                "title" : $0.title,
+                "done" : $0.done
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "tasks")
+    }
+    
+    func loadDatas() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String : Any]] else { return }
+        self.tasks = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done : done)
+        }
+    }
 }
 
 extension ViewController : UITableViewDataSource {
     func tableView(_ tableView : UITableView, numberOfRowsInSection section : Int) -> Int {
-        return 0    }
+        return self.tasks.count    }
     
     func tableView(_ tableView : UITableView, cellForRowAt indexPath : IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for : indexPath)
         let task = self.tasks[indexPath.row]
         cell.textLabel?.text = task.title
+        if task.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
 }
 
-
-
-
-
+extension ViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = self.tasks[indexPath.row]
+        task.done = !task.done
+        self.tasks[indexPath.row] = task
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
