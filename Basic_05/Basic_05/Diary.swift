6f69404d7368ad3b658dfdd8d445b9537e62b89f
k906506 <k906506@naver.com>
2022-09-07 10:24:19 +0900

    [feat] Diary 데이터 클래스 추가

---
+++ Basic_05/Basic_05/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Basic_05
+//
+//  Created by 고도 on 2022/09/07.
+//
+
+import Foundation
+
+struct Diary {
+    var title : String
+    var contents : String
+    var date : Date
+    var isStar: Bool
+}
