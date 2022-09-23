f2a19520cf64fb0b91ac728fd74c246ed4c04cf8
k906506 <k906506@naver.com>
2022-09-23 22:22:59 +0900

    [feat] Basic 08 프로젝트 초기 세팅

---
+++ Basic_08/Basic_08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_08
+//
+//  Created by 고도 on 2022/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
