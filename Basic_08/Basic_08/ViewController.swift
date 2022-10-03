9c8fa0b3f1697e657d24c9e57ba8a52b56519174
k906506 <k906506@naver.com>
2022-10-03 15:33:34 +0900

    [feat] 하단에 PieChartView 표시하고 일일 발생자 수 내림차순으로 정렬되도록 구현

--- Basic_08/Basic_08/ViewController.swift
+++ Basic_08/Basic_08/ViewController.swift
@@ -7,13 +7,122 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Alamofire
+import Charts
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    
+    let apiKey = "AT7mruCLHQV8geJZRN6fx1zwIUdbiYapc"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return } // self에 대한 옵셔널 바인딩
+            
+            switch result {
+            case let.success(result):
+                debugPrint(result)
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configureChartView(covidOverviewList: covidOverviewList)
+                
+            case let.failure(error):
+                debugPrint("error \(error)")
+            }
+            
+        })
     }
-
-
+    
+    func makeCovidOverviewList(
+        cityCovidOverview: CityCovidOverview
+    ) -> [CovidOverview] {
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.daegu,
+            cityCovidOverview.incheon,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.sejong,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.jeju
+        ]
+    }
+    
+    func configureChartView(covidOverviewList: [CovidOverview]) -> Void {
+        let entries = covidOverviewList
+            .sorted(by: { $0.incDecK > $1.incDecK})
+            .compactMap { [weak self] overview -> PieChartDataEntry? in
+            guard let self = self else { return nil }
+            return PieChartDataEntry(
+                value: self.removeFormatString(string: "\(overview.incDecK)"),
+                label: overview.countryNm,
+                data: overview
+            )
+        }
+        
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        
+        dataSet.colors = ChartColorTemplates.vordiplom() +
+        ChartColorTemplates.joyful() +
+        ChartColorTemplates.liberty() +
+        ChartColorTemplates.pastel() +
+        ChartColorTemplates.material()
+        
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+    }
+    
+    func removeFormatString(string: String) -> Double {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    func configureStackView(koreaCovidOverview: CovidOverview) {
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCnt)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.incDecK)명"
+    }
+    
+    func fetchCovidOverview (
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+        let url: String = "https://api.corona-19.kr/korea/beta/"
+        let param = [
+            "serviceKey": "\(apiKey)"
+        ]
+        
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: {response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+        
+    }
 }
 
