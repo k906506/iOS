e53df469f36570c15203700bbca5fd38c815d2c5
k906506 <k906506@naver.com>
2022-12-30 11:59:40 +0900

    [feat] Intermediate 04 프로젝트 초기 세팅

---
+++ Intermediate_04/Intermediate_04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intermediate_04
+//
+//  Created by 고도 on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
