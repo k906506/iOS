ce55fe8a20cb23571c4386bfd338215d83c3276b
k906506 <k906506@naver.com>
2023-01-17 01:41:49 +0900

    [fix] API 호출 결과가 제대로 보여지지 않는 오류 및 Constraint 수정

--- Intermediate_07/Intermediate_07/BeerDetailViewController.swift
+++ Intermediate_07/Intermediate_07/BeerDetailViewController.swift
@@ -21,7 +21,7 @@ class BeerDetailViewController: UITableViewController {
         
         // Header
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 300)
-        let headerView = UIImageView()
+        let headerView = UIImageView(frame: frame)
         let imageURL = URL(string: beer?.imageURL ?? "")
         
         headerView.contentMode = .scaleAspectFit
@@ -42,7 +42,7 @@ extension BeerDetailViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 3:
-            return beer?.foodParing?.count ?? 0
+            return beer?.foodPairing?.count ?? 0
         default:
             return 1
         }
@@ -58,8 +58,8 @@ extension BeerDetailViewController {
         case 2:
             return "Tips"
         case 3:
-            let isFoodParingEmpty = beer?.foodParing?.isEmpty ?? true
-            return isFoodParingEmpty ? nil : "Food Paring"
+            let isFoodPairingEmpty = beer?.foodPairing?.isEmpty ?? true
+            return isFoodPairingEmpty ? nil : "Food Pairing"
         default:
             return nil
         }
@@ -82,7 +82,7 @@ extension BeerDetailViewController {
             cell.textLabel?.text = beer?.brewersTips ?? "팁이 없습니다."
             return cell
         case 3:
-            cell.textLabel?.text = beer?.foodParing?[indexPath.row] ?? ""
+            cell.textLabel?.text = beer?.foodPairing?[indexPath.row] ?? ""
             return cell
         default:
             return cell
