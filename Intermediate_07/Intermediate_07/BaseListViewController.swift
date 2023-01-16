85f30b333c136f8c1c61ab3a3c3cc0e33ee1377a
k906506 <k906506@naver.com>
2023-01-17 01:38:50 +0900

    [feat] API 호출 관련 코드 구현

--- Intermediate_07/Intermediate_07/BaseListViewController.swift
+++ Intermediate_07/Intermediate_07/BaseListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class BaseListViewController: UITableViewController {
     var beerList: [Beer] = []
+    var currentPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,8 @@ class BaseListViewController: UITableViewController {
         // UITableView Set
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
+        
+        fetchBeer(of: currentPage)
     }
 }
 
@@ -45,5 +48,52 @@ extension BaseListViewController {
         
         beerDetailViewController.beer = selectedBeer
         self.show(beerDetailViewController.self, sender: self)
+    }
+}
+
+// Data Fetching
+private extension BaseListViewController {
+    func fetchBeer(of page: Int) {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data) else { // 가져온 데이터를 디코딩
+                print("[ERROR] Network Error : \(error.debugDescription)")
+                return
+            }
+            
+            switch response.statusCode {
+            case (200...299): // 성공
+                self.beerList += beers
+                self.currentPage += 1
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData() // tableView 렌더링
+                }
+            case (400...499): // 클라이언트 에러
+                print("""
+                    [ERROR] Client Error \(response.statusCode)
+                    [Response] \(response)
+                    """)
+            case (500...599): // 서버 에러
+                print("""
+                    [ERROR] Server Error \(response.statusCode)
+                    [Response] \(response)
+                    """)
+            default:
+                print("""
+                    [ERROR] Error \(response.statusCode)
+                    [Response] \(response)
+                    """)
+            }
+        }
+        
+        dataTask.resume()
     }
 }
