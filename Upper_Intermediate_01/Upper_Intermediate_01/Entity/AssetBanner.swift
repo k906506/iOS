0524dec1a775e9301197217c49d3677ed6407e9a
k906506 <k906506@naver.com>
2023-01-18 23:06:40 +0900

    [feat] PageViewController로 PageView 구현

---
+++ Upper_Intermediate_01/Upper_Intermediate_01/Entity/AssetBanner.swift
@@ -0,0 +1,15 @@
+//
+//  AssetBanner.swift
+//  Upper_Intermediate_01
+//
+//  Created by 고도 on 2023/01/18.
+//
+
+import Foundation
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
