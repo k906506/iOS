092238d195caab1468438c00400a94af406c23f4
k906506 <k906506@naver.com>
2023-01-18 21:11:03 +0900

    [feat] Upper Intermediate 01 프로젝트 초기 세팅

---
+++ Upper_Intermediate_01/Upper_Intermediate_01/Upper_Intermediate_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Upper_Intermediate_01App.swift
+//  Upper_Intermediate_01
+//
+//  Created by 고도 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct Upper_Intermediate_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
