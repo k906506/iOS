37c48697fabf09f5bfe2535c45cb68412a8e5280
k906506 <k906506@naver.com>
2023-01-19 11:33:28 +0900

    [feat] 기존 storyboard 제거 후 기본적인 TabView 구현

---
+++ Upper_Intermediate_02/Upper_Intermediate_02/TabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarController.swift
+//  Upper_Intermediate_02
+//
+//  Created by 고도 on 2023/01/19.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    private lazy var todayViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "투데이", image: UIImage(systemName: "mail"), tag: 0)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    private lazy var appViewController: UIViewController = {
+        let viewController = UIViewController()
+        let tabBarItem = UITabBarItem(title: "앱", image: UIImage(systemName: "square.stack.3d.up"), tag: 1)
+        viewController.tabBarItem = tabBarItem
+        
+        return viewController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 어떤 뷰컨트롤러를 표시하려고 하는지
+        viewControllers = [todayViewController, appViewController]
+    }
+}
+
