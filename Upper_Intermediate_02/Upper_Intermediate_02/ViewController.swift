cbb49e5c6459e879e65511b57fb4d797f7b1df2b
k906506 <k906506@naver.com>
2023-01-19 11:10:48 +0900

    [feat] Upper Intermediate 02 프로젝트 초기 세팅

---
+++ Upper_Intermediate_02/Upper_Intermediate_02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upper_Intermediate_02
+//
+//  Created by 고도 on 2023/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
